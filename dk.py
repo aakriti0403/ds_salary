f7668a31226829d86db416ad20a9009328bda073
Aakriti Nag <aakriti0403>
2021-04-07 01:07:55 +0530

    uploaded scraper and run code

---
+++ dk.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 20:56:39 2021
+
+@author: HP
+"""
+
+import glassdoor_scrapper as gs
+import pandas as pd
+
+path="C:/Users/HP/Documents/ds_salary/chromedriver"
+df=gs.get_jobs('data scientist',15,False,path,15)
+df
