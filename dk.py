95d7babbe8a664ca953ed5a80e0fa8e025dc7e13
Aakriti Nag <aakriti0403>
2021-04-07 21:24:46 +0530

    uploading data and data collection script

--- dk.py
+++ dk.py
@@ -9,5 +9,5 @@ import glassdoor_scrapper as gs
 import pandas as pd
 
 path="C:/Users/HP/Documents/ds_salary/chromedriver"
-df=gs.get_jobs('data scientist',15,False,path,15)
+df=gs.get_jobs('data scientist',1000,False,path,15)
 df.to_csv('glassdoor_jobs.csv',index=False)
